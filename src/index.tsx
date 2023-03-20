dca59bd7ed7592e5e8af279db5997299e35d9170
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-03-20 22:40:42 +0530

    Entry point of the app
    Here, we mounted the app component onto the root element. Now we have something which can be rendered in the browser. However, thiss react code cannot be understood by the browser as is. We need babel to convert our react and typescript code into JavaScript.

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import { App } from './App';
+
+// We then mount the App component onto the div element with id=root using the react-dom library
+ReactDOM.render(<App />, document.getElementById('root'));
